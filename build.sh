0b1d08edb8fdfc39f8ef28fffcdb965aa735f050
Marco Nelissen <marco.nelissen@gmail.com>
2022-01-30 19:54:24 -0800

    Some tweaks for Vue UI handling
    - don't check in the package-lock.json file, it is generated by npm
    - update check.sh to include ui/build.sh and exclude ui/node_modules
    - add node version check, since this requires node > 12.7.0
    - add package-lock.json and the "new" folder to .gitignore
    - create the .tgz archive in the ui folder instead of the project root
    - fix dist folder being moved into the new folder after a 2nd build
    - remove github action that uploads the ui archive as a release

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ BASEDIR="$(dirname "$0")"
 
 cd "$BASEDIR"
 npm run build
+rm -rf new
 mv dist new
-tar -zcf ../teslausb-ui.tgz -C . --exclude="*.map" ./new
+tar -zcf teslausb-ui.tgz -C . --exclude="*.map" ./new
